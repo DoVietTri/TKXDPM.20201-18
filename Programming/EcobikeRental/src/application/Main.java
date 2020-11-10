6052a5a8fb3db2d15cd044cf623173299ea6141f
meomam99 <meomam99@gmail.com>
2020-11-10 12:50:45 +0700

    test app

--- Programming/EcobikeRental/src/application/Main.java
+++ Programming/EcobikeRental/src/application/Main.java
@@ -12,9 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,700,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Hello Do Viet Tri, are you see these changes ?");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
