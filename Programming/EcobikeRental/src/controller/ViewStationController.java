ef75be7163cf4441b2d2bd624b4a9801349b1446
trinhnn040999 <53521965+trinhnn040999@users.noreply.github.com>
2020-11-29 13:57:20 +0700

    Update ViewStationController.java

--- Programming/EcobikeRental/src/controller/ViewStationController.java
+++ Programming/EcobikeRental/src/controller/ViewStationController.java
@@ -114,9 +114,7 @@ public class ViewStationController  implements Initializable{
 			stage.setTitle("Bike Info");
 			stage.show();
 
-	} 	 catch (IOException e) {
-			e.printStackTrace();
-		}
+	} 	 
 
 	}
 	
